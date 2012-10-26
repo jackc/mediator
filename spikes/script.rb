45bad06f41c30829ff85999aa26720e4f0a343b6
Jack Christensen <jack@jackchristensen.com>
2012-10-26 18:10:31 -0500

    Added initial spike of script clone.

---
+++ spikes/script.rb
@@ -0,0 +1,40 @@
+require 'pty'
+require 'eventmachine'
+
+system("stty raw -echo")
+
+$pty_reader, $pty_writer, $pty_pid = PTY.spawn("/bin/bash")
+
+$logger = open("script.log", "wb")
+
+class TerminalReader < EventMachine::Connection
+  def receive_data(data)
+    $pty_writer.print data
+  end
+
+  def unbind
+    EM.stop
+  end
+end
+
+class PTYReader < EventMachine::Connection
+  def receive_data(data)
+    $stdout.print data
+    $logger.print data
+  end
+
+  def unbind
+    EM.stop
+  end
+end
+
+EM.run do
+  EM.attach $stdin, TerminalReader
+  EM.attach $pty_reader, PTYReader
+end
+
+at_exit do
+  system("stty -raw echo")
+end
+
+__END__
